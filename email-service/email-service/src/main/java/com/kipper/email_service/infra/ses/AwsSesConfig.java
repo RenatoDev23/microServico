48f2828ac12731e395ab62d831c84145a824332e
Renato <renatorenatex20@gmail.com>
2026-04-02 12:44:16 -0300

    finalizando o envio de email

---
+++ email-service/email-service/src/main/java/com/kipper/email_service/infra/ses/AwsSesConfig.java
@@ -0,0 +1,18 @@
+package com.kipper.email_service.infra.ses;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+@Configuration
+public class AwsSesConfig {
+	
+	@Bean
+	public AmazonSimpleEmailService amazonSimpleEmailService() {
+		return AmazonSimpleEmailServiceClientBuilder.standard()
+				.withRegion("us-east-1") // 👈 ADICIONE ISSO
+				.build();
+	}
+}
