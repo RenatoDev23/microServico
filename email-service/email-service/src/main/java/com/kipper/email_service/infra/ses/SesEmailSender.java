4e4afe53eafe13269a0fd403bc0e3e8d2c801767
Renato <renatorenatex20@gmail.com>
2026-04-02 11:27:01 -0300

    criando a class SES

--- email-service/email-service/src/main/java/com/kipper/email_service/infra/ses/SesEmailSender.java
+++ email-service/email-service/src/main/java/com/kipper/email_service/infra/ses/SesEmailSender.java
@@ -3,6 +3,11 @@ package com.kipper.email_service.infra.ses;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 import com.kipper.email_service.adapters.EmailSenderGateway;
 
 public class SesEmailSender implements EmailSenderGateway {
@@ -17,8 +22,16 @@ public class SesEmailSender implements EmailSenderGateway {
 	}
 	
 
+	// request com cadastro do email e o que este email vai enviar (subject e o body
 	@Override
 	public void sendEmail(String to, String subject, String body) {
+		SendEmailRequest request = new SendEmailRequest()
+				.withSource("renatorenatex20@gmail.com")
+				.withDestination(new Destination().withToAddresses(to))
+				.withMessage(new Message()
+						.withSubject(new Content(subject))
+						.withBody(new Body().withText(new Content(body)))
+						);
 		
 	}
 	
